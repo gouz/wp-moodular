455e2468dbfe5f76a338b5dc64bc17f205b64053
bpongy <bpongy@axome.com>
2014-02-26 12:09:18 +0100

    popup, button

--- index.php
+++ index.php
@@ -8,3 +8,21 @@
  * Author URI: http://www.gougouzian.fr/
  * License: MIT
  */
+
+
+function add_moodular_button() {
+	$popup_url = 'moodular-popup';
+	$title = 'Insérer un diaporama dans la page';
+
+	$bt = "<a title='{$title}' href='#TB_inline?width=640&height=650&inlineId={$popup_url}' class='thickbox button add_media' style='padding-left: 0px; padding-right: 0px;' title='{$title}'> <img src='data:image/svg+xml;base64,PD94bWwgdmVyc2lvbj0iMS4wIiBlbmNvZGluZz0idXRmLTgiPz48IURPQ1RZUEUgc3ZnIFBVQkxJQyAiLS8vVzNDLy9EVEQgU1ZHIDEuMS8vRU4iICJodHRwOi8vd3d3LnczLm9yZy9HcmFwaGljcy9TVkcvMS4xL0RURC9zdmcxMS5kdGQiPjxzdmcgdmVyc2lvbj0iMS4xIiB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHhtbG5zOnhsaW5rPSJodHRwOi8vd3d3LnczLm9yZy8xOTk5L3hsaW5rIiB4PSIwcHgiIHk9IjBweCIgd2lkdGg9IjIwcHgiIGhlaWdodD0iMjBweCIgdmlld0JveD0iMCAwIDIwIDIwIiBlbmFibGUtYmFja2dyb3VuZD0ibmV3IDAgMCAyMCAyMCIgeG1sOnNwYWNlPSJwcmVzZXJ2ZSI+PGcgaWQ9IkNhbHF1ZV8xIj48cmVjdCB4PSI2IiB5PSI3IiBmaWxsPSJub25lIiBzdHJva2U9IiMwMDAwMDAiIHN0cm9rZS13aWR0aD0iMS43MzA3IiBzdHJva2UtbWl0ZXJsaW1pdD0iMTAiIHdpZHRoPSI4IiBoZWlnaHQ9IjYiLz48L2c+PGcgaWQ9IkNhbHF1ZV8yIj48cG9seWdvbiBwb2ludHM9IjMuNywxMi42IDEsOS45IDMuNyw3LjMgIi8+PHBvbHlnb24gcG9pbnRzPSIxNi4zLDEyLjYgMTksOS45IDE2LjMsNy4zICIvPjwvZz48L3N2Zz4=' style='height: 20px; position: relative; top: -2px;'></a>";
+
+	echo $bt;
+}
+add_action('media_buttons', 'add_moodular_button', 11);
+
+
+
+function moodular_popup() {
+	include('moodular-popup.php');
+}
+add_action('admin_footer', 'moodular_popup');
---
+++ moodular-popup.php
@@ -0,0 +1,62 @@
+<script>
+jQuery(document).ready(function() {
+    jQuery(".insert-moo").click(function() {
+    	var vitesse = parseFloat(jQuery('#vitesse').val().replace(',', '.')) * 1000;
+    	var aleatoire = jQuery('#moodular-random:checked').val();
+    	if( aleatoire==undefined )
+    		aleatoire = '';
+    	else { aleatoire = ' random="' + aleatoire + '"'; }
+    	var shortcode_moodular = '[slider id="'+jQuery('#s-moodular').val()+'" v="'+vitesse+'" transition="'+jQuery('#s-transition').val()+'" ctrl="'+jQuery('#s-ctrl').val()+'" aff="'+jQuery('#s-aff').val()+'" '+aleatoire+']';
+		var winmoo = window.dialogArguments || opener || parent || top;
+		winmoo.send_to_editor(shortcode_moodular);
+    });
+});
+</script>
+<div style="display:none;" id="moodular-popup">
+    <div id="moodular-help">
+    <?php
+    	$html = '<table class="form-table"><tbody>';
+		$args = array (
+			'post_type'              => 'moodular',
+			'post_parent'            => 0,
+			'pagination'             => false,
+			'posts_per_page'         => '999',
+			'orderby'                => 'menu_order',
+		);
+		$moodular = new WP_Query( $args );
+		$moods = '<select id="s-moodular">';
+		if ( $moodular->have_posts() ) {
+			while ( $moodular->have_posts() ) {
+				$moodular->the_post();
+				$test =& get_children(array('post_parent' => get_the_ID(), 'post_type' => 'moodular'));
+				$nb = count($test);
+				if( $nb )
+					$moods .= '<option value="'.get_the_ID().'">'.get_the_title().' ('.count($test).' images)</option>';
+			}
+		}
+		$moods .= '</select>';
+		wp_reset_postdata();
+
+    	$html .= '<tr valign="top"><th width="33%" scope="row">Sélectionnez votre slider</th>';
+		$html .= '<td>'.$moods.'</td></tr>';
+    	
+    	$html .= '<tr valign="top"><th width="33%" scope="row">Vitesse</th>';
+		$html .= '<td><input id="vitesse" type="text" class="small-text" value="4" /> secondes (0 pour fonctionnement manuel)</td></tr>';
+    	
+    	$html .= '<tr valign="top"><th width="33%" scope="row">Transition</th>';
+		$html .= '<td><select id="s-transition"><option value="1">fondu</option><option value="2">slide à gauche</option></select></td></tr>';
+    	
+    	$html .= '<tr valign="top"><th width="33%" scope="row">Contrôles</th>';
+		$html .= '<td><select id="s-ctrl"><option value="1">Flèches</option><option value="2">Puces</option></select><!-- <option value="3">Miniatures</option> --></td></tr>';
+    	
+    	$html .= '<tr valign="top"><th width="33%" scope="row">Afficher</th>';
+		$html .= '<td><select id="s-aff"><option value="1">Image uniquement</option><option value="2">Image et titre</option><option value="3">Image, titre et texte</option></select></td></tr>';
+    	
+    	$html .= '<tr valign="top"><th width="33%" scope="row">Ordre</th>';
+		$html .= '<td><label for="moodular-random"><input type="checkbox" value="1" id="moodular-random" name="moodular-random"> aléatoire</label></td></tr>';
+		
+		$html .= '</tbody></table><p class="submit"><button class="button button-primary button-large insert-moo">Insérer le slider</button></p>';
+        echo $html;
+    ?>
+    </div>
+</div>
