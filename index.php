244e56d945d37a62f135ef3d509d26c871182ae4
bpongy <bpongy@axome.com>
2014-02-26 10:47:40 +0100

    let's rock

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Plugin Name: Moodular
+ * Plugin URI: http://www.gougouzian.fr/projects/jquery/moodular/
+ * Description: Moodular
+ * Version: 0.1
+ * Author: Gouz
+ * Author URI: http://www.gougouzian.fr/
+ * License: MIT
+ */
