3ea76b16a8476f5045d2d53779374f66e3f269e9
Benjamin PONGY <benjamin.pongy@gmail.com>
2014-11-06 10:04:40 +0100

    Update index.php
    [*] description

--- index.php
+++ index.php
@@ -2,8 +2,7 @@
 /**
  * Plugin Name: Moodular
  * Plugin URI: http://www.gougouzian.fr/projects/jquery/moodular/
- * Description: Easily create carousels with Moodular and integrate them in your
- * posts
+ * Description: Easily create carousels with Moodular and integrate them in your posts
  * Version: 0.3
  * Author: Benjamin PONGY, Sylvain GOUGOUZIAN, Nicolas SOROSAC
  * Author URI: http://www.axome.com/
